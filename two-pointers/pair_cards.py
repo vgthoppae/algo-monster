88254eb903c05b19ec4f7ca408003ccd2921bd33
vgthoppae <vgthoppae@gmail.com>
2025-12-16 22:46:18 -0500

    2p-pair-cards

--- two-pointers/pair_cards.py
+++ two-pointers/pair_cards.py
@@ -1,23 +1,16 @@
 def least_consecutive_cards_to_match(cards: list[int]) -> int:
   if len(cards) < 2: return -1 #[3, 4, 2, 3, 4, 7]
-  left, right = 0, 1
   window= { #latest position of each card
    cards[0]: 0
   }
-  least_cards = 10000000
+  least_cards = 10000001
 
-  while left < right:
+  for right in range(1, len(cards)):
     if cards[right] in window:
-      left += 1
-      least_cards = min(least_cards, right - window[right] + 1)
+      least_cards = min(least_cards, right - window[cards[right]] + 1)
     window[cards[right]] = right
-    if right < len(cards)-1:
-      right += 1
-    else:
-      left += 1
-      right = left + 1
 
-  return least_cards
+  return least_cards if least_cards < 10000001 else -1
 
 
 if __name__ == "__main__":
